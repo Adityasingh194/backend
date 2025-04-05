c54a6086a9ea75a07ebc34ccc2a1294d75698d79
Adityasingh194 <sadityakumar194@gmail.com>
2025-04-05 12:28:01 +0530

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -1,27 +1,13 @@
-// feedback.js
 import mongoose from "mongoose";
 
 const feedbackSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  event: {
-    type: String,
-    required: true,
-  },
-  reviewText: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+  name: { type: String, required: true },
+  event: { type: String, required: true },
+  reviewText: { type: String, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 const Feedback = mongoose.model("Feedback", feedbackSchema);
-
 export default Feedback;
 
 
